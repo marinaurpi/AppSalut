c83117f6212d74748c62514db8b14568928895f2
LAPTOP-A1QJH46E\marin <marinaurpi267@gmail.com>
2021-05-27 14:11:40 +0200

    update app diseny

--- Assets/Scripts/HomeScript.cs
+++ Assets/Scripts/HomeScript.cs
@@ -5,22 +5,30 @@ using UnityEngine;
 public class HomeScript : MonoBehaviour
 {
     public GameObject welcome, test1;
+    public static bool welcomeActive = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (welcomeActive)
+        {
+            welcome.SetActive(true);
+        }
+        else
+        {
+            welcome.SetActive(false);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 
     public void welcomeState()
     {
         welcome.SetActive(false);
+        welcomeActive = false;
     }
 
     public void test1State()
--- Assets/Scripts/changePanelScript.cs
+++ Assets/Scripts/changePanelScript.cs
@@ -8,7 +8,7 @@ public class changePanelScript : MonoBehaviour
 {
     public RectTransform actualPanel;
     public RectTransform[] Panels;
-    static public string lastPanelName = "ansietatPanel";
+    static public string lastPanelName= "ansietatPanel";
     RectTransform lastPanel;
     public Text actualText;
     public Text[] otherText;
@@ -18,7 +18,6 @@ public class changePanelScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
     }
 
     // Update is called once per frame
@@ -26,32 +25,64 @@ public class changePanelScript : MonoBehaviour
     {
         
     }
-    public void goNext()
+    public void goNextOrdre()
     {
+        actualPanel.gameObject.SetActive(true);
+
         var indexActual = 0;
         var indexLast = 0;
-        for (var j = 0; j < Panels.Length; j++)
-        {
+         
+            for (var j = 0; j < Panels.Length; j++)
+            {
+            /*if (lastPanelName == ""){
+                if (Panels[j].name == "missionsPanel")
+                {
+                    lastPanel = Panels[j];
+                    indexLast = j;
+                }
+                else if (Panels[j].name == "ansietatPanel")
+                {
+                    lastPanel = Panels[j];
+                    indexLast = j;
+                }
+                else if (Panels[j].name == "configuracioPanel")
+                {
+                    lastPanel = Panels[j];
+                    indexLast = j;
+                }
+
+            }
+            else
+            {
+                if (Panels[j].name == lastPanelName)
+                {
+                    lastPanel = Panels[j];
+                    indexLast = j;
+                }
+            }*/
+
             if (Panels[j].name == lastPanelName)
             {
                 lastPanel = Panels[j];
                 indexLast = j;
             }
-            if (Panels[j].name == actualPanel.name)
-            {
-                indexActual = j;
-            }
-        }
 
-        if (indexActual > indexLast)
-        {
-            actualPanel.DOAnchorPos(Vector2.zero, 0.25f);
-            lastPanel.DOAnchorPos(new Vector2(-3500, 0), 0.25f);
-        }
-        else
-        {
-            actualPanel.DOAnchorPos(Vector2.zero, 0.25f);
-            lastPanel.DOAnchorPos(new Vector2(3500, 0), 0.25f);
+            if (Panels[j].name == actualPanel.name)
+                {
+                    indexActual = j;
+                }
+            
+
+            if (indexActual > indexLast)
+            {
+                actualPanel.DOAnchorPos(Vector2.zero, 0.25f);
+                lastPanel.DOAnchorPos(new Vector2(-3500, 0), 0.25f);
+            }
+            else
+            {
+                actualPanel.DOAnchorPos(Vector2.zero, 0.25f);
+                lastPanel.DOAnchorPos(new Vector2(3500, 0), 0.25f);
+            }
         }
 
         actualText.color = new Color32(236, 199, 87, 255);
@@ -59,17 +90,48 @@ public class changePanelScript : MonoBehaviour
 
         for (var i = 0; i < otherText.Length; i++)
         {
-            otherText[i].color = Color.black;
+            otherText[i].color = Color.white;
             otherText[i].font = otherFont;
         }
 
+        lastPanel.gameObject.SetActive(false);
         lastPanelName = actualPanel.name;
+
+    }
+
+    public void goNext()
+    {
+        actualPanel.gameObject.SetActive(true);
+        actualPanel.DOAnchorPos(Vector2.zero, 0.25f);
+        Panels[0].DOAnchorPos(new Vector2(-3500, 0), 0.25f);
+        Panels[0].gameObject.SetActive(false);
+
+        actualText.color = new Color32(236, 199, 87, 255);
+        actualText.font = actualFont;
+
+        for (var i = 0; i < otherText.Length; i++)
+        {
+            otherText[i].color = Color.white;
+            otherText[i].font = otherFont;
+        }
 
     }
 
     public void goBack()
     {
+        actualPanel.gameObject.SetActive(true);
         actualPanel.DOAnchorPos(Vector2.zero, 0.25f);
+        Panels[1].DOAnchorPos(new Vector2(3500, 0), 0.25f);
+        Panels[1].gameObject.SetActive(false);
+
+        actualText.color = new Color32(236, 199, 87, 255);
+        actualText.font = actualFont;
+
+        for (var i = 0; i < otherText.Length; i++)
+        {
+            otherText[i].color = Color.white;
+            otherText[i].font = otherFont;
+        }
 
     }
 }
